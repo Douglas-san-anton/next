e61ae60d5a72ab3db7c5f2e7767f257c85abab00
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-04-03 18:49:21 -0300

    navbar created

---
+++ next-app/src/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from '@/styles/Navbar.module.css'
+import menu from '@/assets/menu.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.nav}>
+
+      <div className={styles.nav__container}>
+
+        <h1 className={styles.nav__logo}>Dusandev</h1>
+
+        <label htmlFor="menu" className={styles.nav__label}>
+          <Image src={menu} alt="icon" className={styles.nav__img} />
+        </label>
+        <input type="checkbox" id="menu" className={styles.nav__input} />
+
+        <div className={styles.nav__menu}>
+          <Link href="#" className={styles.nav__item}>Home</Link>
+          <Link href="#" className={styles.nav__item}>About</Link>
+          <Link href="#" className={styles.nav__item}>Skills</Link>
+          <Link href="#" className={styles.nav__item}>Work</Link>
+          <Link href="#" className={styles.nav__item}>Contact</Link>
+        </div>
+
+      </div>
+
+    </nav>
+  )
+}
--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,24 +1,9 @@
-import styles from '@/styles/HomePage.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-import menu from '@/assets/menu.svg'
+import { Navbar } from '@/components/navbar/Navbar'
 
 function HomePage() {
   return (
     <main>
-      <nav className={styles.nav}>
-        <div className={styles.nav__container}>
-          <h1 className={styles.nav__logo}>Dusandev</h1>
-          {/* <input value={search} onChange={searcher} type="text" placeholder='Buscar' className={styles.search} /> */}
-
-          <label htmlFor="menu" className={styles.nav__label}>
-            <Image src={menu} alt="icon" className={styles.nav__img} />
-          </label>
-          <input type="checkbox" id="menu" className={styles.nav__input} />
-
-        </div>
-      </nav>
-
+      <Navbar />
     </main>
   )
 }
