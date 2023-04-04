52b5597662b9df65f17635375fdceb5c907c32ad
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-04-04 15:33:41 -0300

    modification of styles

--- next-app/src/components/Home.tsx
+++ next-app/src/components/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from '@/styles/Home.module.scss'
+import Image from 'next/image'
+import logo from '@/assets/logo.png'
 
 const Home = () => {
   return (
-    <main className={styles.main}>Home</main>
+    <main className={styles.main}>
+      <h1>Hola soy douglas Fullstack developer</h1>
+      <Image src={logo} alt='logo' width={200} />
+    </main>
   )
 }
 
