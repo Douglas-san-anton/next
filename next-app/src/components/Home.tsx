d820ad0144f394332b64230878f5d5bed33d3bf7
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-04-07 18:03:20 -0300

    new environment variables added in styles

--- next-app/src/components/Home.tsx
+++ next-app/src/components/Home.tsx
@@ -8,8 +8,8 @@ const Home = () => {
       <section className={styles.about}>
         <div className={styles.about__container}>
           <div className={styles.about__text}>
-            <h1>Hola soy douglas Fullstack developer</h1>
-            <p> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
+            <h1 className={styles.about__title}>Hola soy douglas Fullstack developer</h1>
+            <p className={styles.about__p}> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
               Como Freelance, puedo ofrecerte habilidades y experiencia en desarrollo web de alta calidad. Puedes estar seguro de que mi trabajo no solo cumplirá, sino que superará tus necesidades y objetivos.
 
               Mi pasión por la tecnología me ha llevado a convertirme en un experto en el desarrollo de aplicaciones web. Mi portafolio, disponible en https://dusandev.netlify.app/, demuestra mi capacidad para crear soluciones únicas y efectivas que impulsan el éxito empresarial.</p>
@@ -28,8 +28,8 @@ const Home = () => {
       <section className={styles.skills}>
         <div className={styles.skills__container}>
           <div className={styles.skills__text}>
-            <h1>Hola soy douglas Fullstack developer</h1>
-            <p> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
+            <h1 className={styles.skills__title}>Hola soy douglas Fullstack developer</h1>
+            <p className={styles.skills__p}> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
               Como Freelance, puedo ofrecerte habilidades y experiencia en desarrollo web de alta calidad. Puedes estar seguro de que mi trabajo no solo cumplirá, sino que superará tus necesidades y objetivos.
 
               Mi pasión por la tecnología me ha llevado a convertirme en un experto en el desarrollo de aplicaciones web. Mi portafolio, disponible en https://dusandev.netlify.app/, demuestra mi capacidad para crear soluciones únicas y efectivas que impulsan el éxito empresarial.</p>
@@ -48,8 +48,8 @@ const Home = () => {
       <section className={styles.work}>
         <div className={styles.work__container}>
           <div className={styles.work__text}>
-            <h1>Hola soy douglas Fullstack developer</h1>
-            <p> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
+            <h1 className={styles.work__title}>Hola soy douglas Fullstack developer</h1>
+            <p className={styles.work__p}> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
               Como Freelance, puedo ofrecerte habilidades y experiencia en desarrollo web de alta calidad. Puedes estar seguro de que mi trabajo no solo cumplirá, sino que superará tus necesidades y objetivos.
 
               Mi pasión por la tecnología me ha llevado a convertirme en un experto en el desarrollo de aplicaciones web. Mi portafolio, disponible en https://dusandev.netlify.app/, demuestra mi capacidad para crear soluciones únicas y efectivas que impulsan el éxito empresarial.</p>
