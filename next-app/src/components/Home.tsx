177e31d15f77c32d408091f1c6fb1786816aa1f6
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-04-07 16:23:12 -0300

    added skills section

--- next-app/src/components/Home.tsx
+++ next-app/src/components/Home.tsx
@@ -4,7 +4,7 @@ import styles from '@/styles/Home.module.scss'
 
 const Home = () => {
   return (
-    <main className={styles.main_container}>
+    <main className={styles.main__container}>
       <section className={styles.about}>
         <div className={styles.about__container}>
           <div className={styles.about__text}>
@@ -22,7 +22,26 @@ const Home = () => {
               </video>
             </div>
           </div>
+        </div>
+      </section>
 
+      <section className={styles.skills}>
+        <div className={styles.skills__container}>
+          <div className={styles.skills__text}>
+            <h1>Hola soy douglas Fullstack developer</h1>
+            <p> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
+              Como Freelance, puedo ofrecerte habilidades y experiencia en desarrollo web de alta calidad. Puedes estar seguro de que mi trabajo no solo cumplirá, sino que superará tus necesidades y objetivos.
+
+              Mi pasión por la tecnología me ha llevado a convertirme en un experto en el desarrollo de aplicaciones web. Mi portafolio, disponible en https://dusandev.netlify.app/, demuestra mi capacidad para crear soluciones únicas y efectivas que impulsan el éxito empresarial.</p>
+          </div>
+
+          <div className={styles.skills__video_container}>
+            <div className={styles.skills__video}>
+              <video autoPlay muted loop>
+                <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type='video/mp4' />
+              </video>
+            </div>
+          </div>
         </div>
       </section>
     </main>
