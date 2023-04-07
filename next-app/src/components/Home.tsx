fe7f18225176c76197080d82c60064fded2cf0ca
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-04-07 16:55:36 -0300

    added work section

--- next-app/src/components/Home.tsx
+++ next-app/src/components/Home.tsx
@@ -44,6 +44,26 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      <section className={styles.work}>
+        <div className={styles.work__container}>
+          <div className={styles.work__text}>
+            <h1>Hola soy douglas Fullstack developer</h1>
+            <p> ¿Estás buscando un FullStack Developer excepcional que supere tus expectativas? ¡Aquí estoy!
+              Como Freelance, puedo ofrecerte habilidades y experiencia en desarrollo web de alta calidad. Puedes estar seguro de que mi trabajo no solo cumplirá, sino que superará tus necesidades y objetivos.
+
+              Mi pasión por la tecnología me ha llevado a convertirme en un experto en el desarrollo de aplicaciones web. Mi portafolio, disponible en https://dusandev.netlify.app/, demuestra mi capacidad para crear soluciones únicas y efectivas que impulsan el éxito empresarial.</p>
+          </div>
+
+          <div className={styles.work__video_container}>
+            <div className={styles.work__video}>
+              <video autoPlay muted loop>
+                <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type='video/mp4' />
+              </video>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
